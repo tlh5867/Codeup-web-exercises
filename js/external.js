f671f5d49e2f5b555ff4ecc98ea5da0505321dd1
tomashipolito <tlhipolito67@gmail.com>
2017-08-15 09:23:36 -0500

    added external javascript added alerts to html

---
+++ js/external.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log('Hello from external JavaScript!');
+
+alert('what are you doing on this site, get back to work');
+
+confirm('WELCOME TO MY WEBSITE!!');
+
+prompt('What is your favorite color?');
