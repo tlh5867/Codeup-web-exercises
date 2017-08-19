1136b6458fe0db9968b98ed8afe930e1b17b69a6
tomashipolito <tlhipolito67@gmail.com>
2017-08-19 09:07:09 -0500

    used for each in the arrays

--- js/iterating.js
+++ js/iterating.js
@@ -9,11 +9,20 @@
 
 
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-var names = ["Tomas", "Brittany", "Jessica","Ranea"];
+var names = new Array ("Tomas", "Brittany", "Jessica","Ranea");
+
+console.log(names.length); x++
+
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+})();
 
 
-// for(var i = 0, i < names.length, i ++);(
-//     console.log(names))
+
+ for (var i = 0, i < names.length, i ++); {}
+    console.log(names[x])
 
 
 
@@ -25,6 +34,8 @@ var names = ["Tomas", "Brittany", "Jessica","Ranea"];
 
     });
 
+// console.log(names.length)
+
 
 
 
@@ -33,13 +44,6 @@ var names = ["Tomas", "Brittany", "Jessica","Ranea"];
 
     // TODO: Create a log statement that will log the number of elements in the names array.
 
-// console.log(names.length)
-
-    console.log(names[0])
-    console.log(names[1])
-    console.log(names[2])
-    console.log(names[3])
-})();
 
 
 
--- js/practice.js
+++ js/practice.js
@@ -39,16 +39,45 @@
 
  // Continue Loops
 
- 'use strict';
+ // 'use strict';
+ //
+ // for (var i = 8; i < 100; i++) {
+ //
+ //     if (i % 2 !== 0) {
+ //         // number isn't even
+ //         // odd numbers aren't as cool
+ //         // skip the rest of the loop and continue with the next iteration
+ //         continue;
+ //     }
+ //
+ //     console.log('Here is a lovely even number: ' + i);
+ // }
 
- for (var i = 8; i < 100; i++) {
 
-     if (i % 2 !== 0) {
-         // number isn't even
-         // odd numbers aren't as cool
-         // skip the rest of the loop and continue with the next iteration
-         continue;
-     }
 
-     console.log('Here is a lovely even number: ' + i);
- }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
