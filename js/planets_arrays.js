efc2dfd7567715461e2317cabcd78d940ef3e493
tomashipolito <tlhipolito67@gmail.com>
2017-08-21 11:22:44 -0500

    completed the planet exercise

---
+++ js/planets_arrays.js
@@ -0,0 +1,98 @@
+// "use strict";
+//
+//
+// var colors= ['red','orange','yellow','green','blue','indigo','violet','red'];
+//
+// console.log(colors.indexOf("indigo"));
+// console.log(colors.indexOf("red"));
+// console.log(colors.indexOf("green"));
+// console.log(colors.indexOf("blue"));
+//
+//
+// var colors= ['red','orange','yellow','green','blue','indigo','violet','red'];
+//
+//
+// colors.splice(3,4);
+//
+// console.log(colors);
+//
+// colors.reverse();
+//
+// console.log(colors)
+//
+// colors.sort();
+//
+// console.log(colors)
+
+(function(){
+    "use strict";
+
+    // TODO: Create an array holding the names of the eight planets in our solar system in order, starting closest to the sun.
+    var planets = ["Mercury","Venus","Earth","Mars","Jupitor","Saturn","Uranus","Neptune"];
+
+    // function for logging the planets array
+    function logPlanets() {
+        console.log("Here is the list of planets:");
+        console.log(planets);
+        console.log(planets.length);
+        planets.sort();
+    }
+
+
+    logPlanets();
+
+
+    console.log('Adding "The Sun" to the beginning of the planets array.');
+
+    planets.unshift("The Sun");
+    logPlanets()
+
+    console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push("Pluto");
+
+    logPlanets();
+
+    console.log('Removing "The Sun" from the beginning of the planets array.');
+    // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+
+
+    var planets = ["Mercury","Venus","Earth","Mars","Jupitor","Saturn","Uranus","Neptune"];
+    planets.shift("The Sun");
+    logPlanets();
+
+    console.log('Removing "Pluto" from the end of the planets array.');
+    // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    var planets = ["Mercury","Venus","Earth","Mars","Jupitor","Saturn","Uranus","Neptune"];
+    planets.pop("Pluto");
+    logPlanets();
+
+    console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(planets.indexOf("Earth"));
+
+
+
+
+
+    console.log('Using splice to remove the planet after "Earth".');
+    // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.splice(planets.indexOf("Earth") + 1,1);
+
+
+    logPlanets();
+
+    console.log('Using splice to add back the planet after "Earth".');
+    // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+
+
+    logPlanets();
+
+    console.log("Reversing the order of the planets array.");
+    // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.reverse()
+    logPlanets();
+
+    console.log("Sorting the planets array.");
+    // TODO: Read the console.log() statement above. Write code to perform the step it describes.
+    planets.sort()
+    logPlanets();
+})();
