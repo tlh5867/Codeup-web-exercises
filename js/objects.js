ec41fca81021cc820f60a2b6b4368cd3c26b8312
Tomas Hipolito <tlhipolito67@gmail.com>
2017-08-23 09:39:31 -0500

    completed the objects exercise

--- js/objects.js
+++ js/objects.js
@@ -74,3 +74,35 @@
 
 
 )();
+
+
+// todo:
+// Create an array of objects that represent books.
+// Each book should have a title and an author property.
+// The author property should be an object with a firstName and lastName.
+// Be creative and add at least 5 books to the array
+// var books = [todo];
+
+// log out the books array
+var books= [
+
+    {
+    
+
+
+
+
+
+]
+
+
+console.log(books);
+
+// todo:
+// Loop through the array of books using .forEach and print out the specified information about each one.
+// start loop here
+console.log("Book #" + todo);
+console.log("Title: " + todo);
+console.log("Author: " + todo);
+console.log("---");
+// end loop here
