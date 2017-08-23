163f1f30b27431816c181c3a27517321fa7384fb
Tomas Hipolito <tlhipolito67@gmail.com>
2017-08-23 14:06:30 -0500

    created a js stylesheet for circles completed the exercises for objects

--- js/circle.js
+++ js/circle.js
@@ -6,19 +6,27 @@
         radius: 3,
 
         getArea: function () {
+            var circleArea = Math.PI * Math.pow(circle.radius,2);
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+            return circleArea; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
+            if(doRounding == true){
+                 // Math.round(circle.getArea());
+            var circleDeath = Math.round(circle.getArea());
+            }else{
+                circleDeath = circle.getArea();
+
+            }
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + circleDeath);
         }
     };
 
@@ -31,6 +39,7 @@
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
 
+    circle.radius = 5
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
