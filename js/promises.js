9f4be34c17fa64d87150711cfa275e159e158601
Tomas Hipolito <tlhipolito67@gmail.com>
2017-09-12 15:50:50 -0500

    completed exercise for promises

--- js/promises.js
+++ js/promises.js
@@ -1,52 +1,56 @@
 "use strict";
 
+const wait = (delayTime) => {
+       return new Promise((resolve,reject) =>{
+           setTimeout(() => {
+           resolve();
+        });
+    }, delayTime);
+};
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+// function wait(delayTime) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve()
+//         }, delayTime);
+//     });
+// }
+//
 // wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
-function firstPromise(nextAction){
-    console.log("you\'ll see this after 1 second");
-    nextAction();
-    function firstPromise() {
-        console.log("you\'ll see this after 1 second");
-        setTimeout(
-            () => {},
-            1000
-        );
 
+// function wait(delayTime){
+//     return new Promise(function(resolve, reject){
+//         setTimeout(resolve, delayTime);
+//         setTimeout(reject, 6000)
+//     })
+// }''
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+// wait(7000).then(() => console.log('T-ERROR'));
+//
+//
+//
+//abef9a330a21f5aa1fea01274f483326693f12cd
+//GET /users/:username/events/
+//
+// fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
+const optionalHeaders ={
+    headers: {
+        "Authorization": "token abef9a330a21f5aa1fea01274f483326693f12cd"
     }
+};
 
+const getCommits = (username) => {
+    return fetch(`https://api.github.com/users/${username}/events`,optionalHeaders)
+};
+let githubPromise = getCommits("tlh5867");
 
-
-
-
-// function practiceCoding(/* callback (function )*/ nextAction) {
-//     console.log("I'm practicing wrting code...");
-//     nextAction(); // synchronization
-//     function practiceCoding() {
-//         console.log("I'm practicing writing code...");
-//         setTimeout(
-//             () => {},
-//             2000
-//         );
-//     }
-//
-//     function practiceShortcuts(/* callback (function )*/ nextThingToDo) {
-//         console.log("I'm practicing the refactoring shortcuts");
-//         nextThingToDo();
-//         function practiceShortcuts() {
-//             console.log("I'm practicing the refactoring shortcuts");
-//             setTimeout(
-//                 () => {},
-//                 3000
-//             );
-//         }
-//
-//         function readNextLesson(/* callback (function )*/ next) {
-//             function readNextLesson() {
-//                 console.log("I'm reading the next lesson");
-//                 next();
-//                 setTimeout(
-//                     () => {},
-//                     1500
-//                 );
-//             }}}}
+githubPromise.then((Promise) =>{
+    Promise.json().then( (response) => {
+    console.log(response{0}.created_at);
+})
+});
